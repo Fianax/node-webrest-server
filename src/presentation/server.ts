41bc7dc2106b3fb09467305df9f29c50c7a39a60
cosme <cosmemorales2c@gmail.com>
2024-04-16 10:16:25 +0200

    Añadidas las nuevas rutas CRUD

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router, response } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
+    routers: Router;
     public_path?: string;
 }
 
@@ -11,17 +12,27 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
     constructor(options: Options){
         this.port = options.port;
         this.publicPath = options.public_path ?? 'public';
+        this.routes = options.routers;
     }
 
     async start(){
 
+        //* Middleware
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: true})); // x-www-form-urlencoded
+
         //* Public folder
         this.app.use(express.static(this.publicPath));
 
+        //* Routes
+        this.app.use(this.routes)
+
+        //* SPA
         this.app.get('*', (req, res) => {
             const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
             res.sendFile(indexPath);
